715722872733cc4df56633f1a0a1c8bf385b884a
Pixelgost <thisisadhi@gmail.com>
2024-11-01 17:59:28 -0400

    Creating Entities

---
+++ Backend/src/main/java/com/example/Backend/Patient/Patient.java
@@ -0,0 +1,15 @@
+package com.example.Backend.Patient;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Patient {
+    private String name;
+
+    private int age;
+
+    private String height;
+
+    private int weight;
+
+}
