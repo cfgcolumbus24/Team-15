2896a33a425c53914f861176d5380d8989bec90f
23stummala <tummalasiddarth@gmail.com>
2024-11-01 21:35:13 -0400

    Fixed App.js bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginFinal from './pages/login.tsx';
 import Home from './pages/home.tsx';
-import Option1 from './pages/option1.tsx'; // Import the components for each option
-import Option2 from './pages/option2.tsx';
-import Option3 from './pages/option3.tsx';
 
 function App() {
   return (
@@ -13,9 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginFinal />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/option1" element={<Option1 />} />
-        <Route path="/option2" element={<Option2 />} />
-        <Route path="/option3" element={<Option3 />} />
       </Routes>
     </Router>
   );
