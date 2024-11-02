3ba17e82c5a59c473dccab3672583d6e89ff699b
marleimininger <marleimininger@gmail.com>
2024-11-02 02:14:11 -0400

    merge error commit

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useState } from "react";
 import Sheet from "@mui/joy/Sheet";
 import CssBaseline from "@mui/joy/CssBaseline";
 import Typography from "@mui/joy/Typography";
@@ -8,32 +7,12 @@ import FormLabel from "@mui/joy/FormLabel";
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
 
 const LoginFinal = () => {
   const navigate = useNavigate();
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
-  const url = 'http://10.250.254.224:8080/api/v1/';
 
   const handleLogin = () => {
-
-    console.log(firstName, lastName)
-    axios.post(url + 'clinician/login', {firstName: firstName, lastName: lastName} )
-    .then(response => {
-       console.log(response.data);
-       alert("Sucess!");
-       navigate('/home')
-    }).catch(error => {
-        console.log(error.response.status)
-        if(error.response.status === 401) {
-            alert("Invalid Login Credentials")
-        } else {
-            alert("Something went wrong, try again!")
-        }
-        console.error(error);
-    });
-
+    navigate("/Home");
   };
 
   return (
@@ -84,29 +63,27 @@ const LoginFinal = () => {
           Please sign in to continue.
         </Typography>
         <FormControl>
-          <FormLabel>First Name</FormLabel>
+          <FormLabel>Username</FormLabel>
           <Input
-            name="First Name"
+            name="username"
             type="text"
-            placeholder="john doe"
+            placeholder="johndoe"
             sx={{
               height: '48px', 
               fontSize: '1rem', 
             }}
-            onChange={(e) => {setFirstName(e.target.value)}}
           />
         </FormControl>
         <FormControl>
-          <FormLabel>Last Name</FormLabel>
+          <FormLabel>Password</FormLabel>
           <Input
-            name="lastName"
-            type="text"
-            placeholder="Last Name"
+            name="password"
+            type="password"
+            placeholder="password"
             sx={{
               height: '48px', 
               fontSize: '1rem', 
             }}
-            onChange={(e) => {setLastName(e.target.value)}}
           />
         </FormControl>
         <Button 
