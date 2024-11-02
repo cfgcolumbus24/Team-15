a4c27ad06bff96aaef25044288d7b00d6660320f
Phillip <phillipm@udel.edu>
2024-11-02 06:30:22 -0400

    changes I do not want to add but will overwrite with develop branch

--- Backend/src/main/java/com/example/Backend/BackendApplication.java
+++ Backend/src/main/java/com/example/Backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class BackendApplication {
 
 	public static void main(String[] args) {
