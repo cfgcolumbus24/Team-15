7fa6fb1e54f736501f381b08597422dfc0223321
Pixelgost <thisisadhi@gmail.com>
2024-11-01 23:35:02 -0400

    Finished Patient Controller

--- Backend/src/main/java/com/example/Backend/patient/PatientController.java
+++ Backend/src/main/java/com/example/Backend/patient/PatientController.java
@@ -5,10 +5,7 @@ import com.example.Backend.Clinician.ClinicianRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 
 @RestController
@@ -37,12 +34,89 @@ public class PatientController {
     }
     @GetMapping("/all")
     public List<Patient> getAllPatients() {
-        return (List<Patient>) patientRepository.findAll();
+
+        List<Patient> patientList = (List<Patient>) patientRepository.findAll();
+        patientList.forEach((n) -> {n.getDoctors().forEach((i) -> {i.setClients(null);});});
+        return patientList;
     }
 
     @GetMapping("/{name}")
     public Patient getPatientByName(@PathVariable String name) {
-        return patientRepository.findPatientByName(name);
+        Patient p = patientRepository.findPatientByName(name);
+        p.getDoctors().forEach((n) -> {n.setClients(null);});
+        return p;
+    }
+
+    @GetMapping("/demo/{demographic}")
+    public HashMap<String, Double> getPatientDemographics(@PathVariable String demographic) {
+        HashMap<String, Double> demographics = new HashMap<>();
+        int count = 0;
+        if (demographic.equals("age")) {
+            List<Patient> patientList = new ArrayList<>();
+            patientRepository.findAll().forEach(patientList::add);
+            count = patientList.size();
+            patientList.forEach((n) -> {
+                String ageString = String.valueOf(n.getAge());
+                if (!demographics.containsKey(ageString)) {
+                    demographics.put(ageString, 1.0);
+                } else {
+                    demographics.put(ageString, demographics.get(ageString) + 1);
+                }
+            });
+        } else if (demographic.equals("race")) {
+            List<Patient> patientList = new ArrayList<>();
+            patientRepository.findAll().forEach(patientList::add);
+            count = patientList.size();
+            patientList.forEach((n) -> {
+                String ageString = n.getRace();
+                if (!demographics.containsKey(ageString)) {
+                    demographics.put(ageString, 1.0);
+                } else {
+                    demographics.put(ageString, demographics.get(ageString) + 1);
+                }
+            });
+        } else if (demographic.equals("gender")) {
+            List<Patient> patientList = new ArrayList<>();
+            patientRepository.findAll().forEach(patientList::add);
+            count = patientList.size();
+            patientList.forEach((n) -> {
+                String ageString = n.getGender();
+                if (!demographics.containsKey(ageString)) {
+                    demographics.put(ageString, 1.0);
+                } else {
+                    demographics.put(ageString, demographics.get(ageString) + 1);
+                }
+            });
+        } else if (demographic.equals("weight")) {
+            List<Patient> patientList = new ArrayList<>();
+            patientRepository.findAll().forEach(patientList::add);
+            count = patientList.size();
+            patientList.forEach((n) -> {
+                String ageString = String.valueOf(n.getWeight());
+                if (!demographics.containsKey(ageString)) {
+                    demographics.put(ageString, 1.0);
+                } else {
+                    demographics.put(ageString, demographics.get(ageString) + 1);
+                }
+            });
+        } else {
+            List<Patient> patientList = new ArrayList<>();
+            patientRepository.findAll().forEach(patientList::add);
+            count = patientList.size();
+            patientList.forEach((n) -> {
+                String ageString = n.getSex();
+                if (!demographics.containsKey(ageString)) {
+                    demographics.put(ageString, 1.0);
+                } else {
+                    demographics.put(ageString, demographics.get(ageString) + 1);
+                }
+            });
+        }
+
+        for (String key : demographics.keySet()) {
+            demographics.put(key, demographics.get(key) / count);
+        }
+        return demographics;
     }
 
 
