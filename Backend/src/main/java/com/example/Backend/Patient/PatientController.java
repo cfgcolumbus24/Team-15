1490078a1714e94d536636c5c4b5b17f30d6bfb2
Phillip <phillipm@udel.edu>
2024-11-02 00:28:55 -0400

    controller

--- Backend/src/main/java/com/example/Backend/Patient/PatientController.java
+++ Backend/src/main/java/com/example/Backend/Patient/PatientController.java
@@ -1,13 +1,11 @@
 package com.example.Backend.Patient;
 
-import com.example.Backend.Clinician.Clinician;
 import com.example.Backend.Clinician.ClinicianRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 
 @RestController
@@ -24,8 +22,23 @@ public class PatientController {
     public @ResponseBody String getPatient() {
         return "Hello World!";
     }
+
+
     @GetMapping
     public List<Patient> getAllPatients() {
         return (List<Patient>) patientRepository.findAll();
     }
+
+    // switches out patient
+    @PutMapping("/api/v1/Patients/{id}")
+    public Optional<Patient> replacePatient(@RequestBody Patient newPatient, @PathVariable int id){
+        return Optional.of(patientRepository.findById(id).map(patient -> {
+            patient.setName(newPatient.getName());
+            patient.setDob(newPatient.getDob());
+            return patientRepository.save(patient);
+        }).orElseGet(() -> {
+            return patientRepository.save(newPatient);
+        }));
+    }
+
 }
