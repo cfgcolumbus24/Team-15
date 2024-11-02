5ae867febb2fe2a4d3d3f0049bdcd921cc1b304f
Adewale-1 <Adewaleadenlejnr@gmail.com>
2024-11-02 06:41:54 -0400

    Final Feature for the Plain Text Search Lookup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,9 @@ import LoginFinal from './pages/login.tsx';
 import Home from './pages/home.tsx';
 import Demographics from './pages/demographics.tsx';
 import Patients from './pages/users.tsx';
-import Patient from './pages/patient.tsx'
+import Patient from './pages/patient.tsx';
 import Financials from './pages/financials.tsx';
+import PlainText from './pages/plaintext.tsx';  // Add this line
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/patient/:patientName" element={<Patient />} />
         <Route path="/demographics" element={<Demographics />} />
         <Route path="/financials" element={<Financials />} />
+        <Route path="/plaintext" element={<PlainText />} />
       </Routes>
     </Router>
   );
